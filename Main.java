4efd6e0f480fc0c2a22b6a91b2bedc245f47fdd2
zouhairdragon28-png <zouhairdragon28@gmail.com>
2025-12-18 09:08:38 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
-package projet;
+  package projetM6809;
+ import javax.swing.SwingUtilities;
+ import javax.swing.UIManager;
 
-import javax.swing.SwingUtilities;
+ public class Main {
+     public static void main(String[] args) {
+         try {
+             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+         } catch (Exception e) {}
 
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Memory ram = new Memory();
-            Cpu6809 cpu = new Cpu6809(ram);
-            SimulatorGUI gui = new SimulatorGUI(cpu, ram);
-            gui.setVisible(true);
-        });
-    }
-}
+         SwingUtilities.invokeLater(() -> {
+             Memory mem = new Memory();
+             Cpu6809 cpu = new Cpu6809(mem);
+             SimulatorGUI gui = new SimulatorGUI(cpu, mem);
+             gui.setVisible(true);
+         });
+     }
+ }
