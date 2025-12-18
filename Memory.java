2030bb2899f36a7f0c48f66912db6e7ae5195041
zouhairdragon28-png <zouhairdragon28@gmail.com>
2025-12-18 09:14:01 +0100

    Update Memory.java

--- Memory.java
+++ Memory.java
@@ -1,26 +1,32 @@
- package projet;
+  package projetM6809;
 
 public class Memory {
-    // La mémoire du 6809 fait 64 Ko (65536 octets)
+    // La mémoire contient 65536 octets (64 Ko)
     private byte[] data = new byte[65536];
 
-    public int readByte(int address) {
-        return data[address & 0xFFFF] & 0xFF;
-    }
-
-    public int readWord(int address) {
-        int high = readByte(address);
-        int low = readByte(address + 1);
-        return (high << 8) | low;
-    }
-
-    public void writeByte(int address, int value) {
-        data[address & 0xFFFF] = (byte) value;
+    public Memory() {
+        reset();
     }
 
     public void reset() {
         for (int i = 0; i < data.length; i++) {
-            data[i] = 0;
+            // Règle :
+            // De 0000 à 7FFF (RAM) --> Initialisé à 00
+            // De 8000 à FFFF (ROM) --> Initialisé à FF (Mémoire vide standard)
+            if (i < 0x8000) {
+                data[i] = 0;
+            } else {
+                data[i] = (byte) 0xFF; // C'est pour ça que A devient FF quand on lit en ROM
+            }
         }
+    }
+
+    public int read(int address) {
+        // Lecture d'un octet et conversion en entier positif (0-255)
+        return data[address & 0xFFFF] & 0xFF;
+    }
+
+    public void write(int address, int value) {
+        data[address & 0xFFFF] = (byte) value;
     }
 }
