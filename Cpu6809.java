9d84518250333b7e104b70d1067044c05ad3f48a
zouhairdragon28-png <zouhairdragon28@gmail.com>
2025-12-21 18:15:03 +0100

    Update Cpu6809.java

--- Cpu6809.java
+++ Cpu6809.java
@@ -1,4 +1,5 @@
-   package projetM6809;
+         
+package projetM6809;
 
 public class Cpu6809 {
     private Memory memory;
@@ -6,6 +7,8 @@ public class Cpu6809 {
     private int PC; 
     private int S, U, X, Y; 
     private int A, B, DP, CC; 
+    
+    private boolean programFinished = false; 
     
     private String currentInstructionStr = "RESET"; 
 
@@ -19,92 +22,84 @@ public class Cpu6809 {
         S = 0x0000; U = 0x0000; X = 0x0000; Y = 0x0000;
         A = 0x00; B = 0x00; DP = 0x00;
         CC = 0; 
+        programFinished = false; 
         currentInstructionStr = "RESET";
     }
 
     public void step() {
+        if (programFinished) return; 
+
         int opcode = memory.read(PC);
         PC++;
 
         switch(opcode) {
-            // --- 8 BITS OPERATIONS ---
-            case 0x86: // LDA Imm
-                A = memory.read(PC++); updateFlags(A); 
-                currentInstructionStr = String.format("LDA #$%02X", A); break;
-            case 0xC6: // LDB Imm
-                B = memory.read(PC++); updateFlags(B); 
-                currentInstructionStr = String.format("LDB #$%02X", B); break;
-                
-            // --- INDEXED MODE (,X) ---
-            case 0xA6: // LDA ,X
-                memory.read(PC++); // Skip postbyte (0x84)
-                A = memory.read(X); updateFlags(A);
-                currentInstructionStr = "LDA ,X"; break;
-            case 0xE6: // LDB ,X
-                memory.read(PC++); 
-                B = memory.read(X); updateFlags(B);
-                currentInstructionStr = "LDB ,X"; break;
-            case 0xA7: // STA ,X
-                memory.read(PC++);
-                memory.write(X, A); updateFlags(A);
-                currentInstructionStr = "STA ,X"; break;
-            case 0xE7: // STB ,X
-                memory.read(PC++);
-                memory.write(X, B); updateFlags(B);
-                currentInstructionStr = "STB ,X"; break;
+            // --- 8 BITS (IMMEDIATE) ---
+            // دابا كيكتب ليك الرقم مع التعليمة
+            case 0x86: A = memory.read(PC++); updateFlags(A); 
+                       currentInstructionStr = String.format("LDA #$%02X", A); break;
+            case 0xC6: B = memory.read(PC++); updateFlags(B); 
+                       currentInstructionStr = String.format("LDB #$%02X", B); break;
 
-            // --- DIRECT MODE (<$xx) ---
-            case 0x96: // LDA Direct
-                int addrA = (DP << 8) | memory.read(PC++);
-                A = memory.read(addrA); updateFlags(A);
-                currentInstructionStr = String.format("LDA <$%02X", addrA & 0xFF); break;
-            case 0xD6: // LDB Direct
-                int addrB = (DP << 8) | memory.read(PC++);
-                B = memory.read(addrB); updateFlags(B);
-                currentInstructionStr = String.format("LDB <$%02X", addrB & 0xFF); break;
-            case 0x97: // STA Direct
-                int sAddrA = (DP << 8) | memory.read(PC++);
-                memory.write(sAddrA, A); updateFlags(A);
-                currentInstructionStr = String.format("STA <$%02X", sAddrA & 0xFF); break;
-            case 0xD7: // STB Direct
-                int sAddrB = (DP << 8) | memory.read(PC++);
-                memory.write(sAddrB, B); updateFlags(B);
-                currentInstructionStr = String.format("STB <$%02X", sAddrB & 0xFF); break;
+            // --- INCREMENT / DECREMENT ---
+            case 0x4A: A = (A - 1) & 0xFF; updateFlags(A); currentInstructionStr = "DECA"; break;
+            case 0x5A: B = (B - 1) & 0xFF; updateFlags(B); currentInstructionStr = "DECB"; break;
+            case 0x4C: A = (A + 1) & 0xFF; updateFlags(A); currentInstructionStr = "INCA"; break;
+            case 0x5C: B = (B + 1) & 0xFF; updateFlags(B); currentInstructionStr = "INCB"; break;
+
+            // --- INDEXED MODE ---
+            case 0xA6: memory.read(PC++); A = memory.read(X); updateFlags(A); currentInstructionStr = "LDA ,X"; break;
+            case 0xE6: memory.read(PC++); B = memory.read(X); updateFlags(B); currentInstructionStr = "LDB ,X"; break;
+            case 0xA7: memory.read(PC++); memory.write(X, A); updateFlags(A); currentInstructionStr = "STA ,X"; break;
+            case 0xE7: memory.read(PC++); memory.write(X, B); updateFlags(B); currentInstructionStr = "STB ,X"; break;
+
+            // --- DIRECT MODE ---
+            case 0x96: int aA=(DP<<8)|memory.read(PC++); A=memory.read(aA); updateFlags(A); 
+                       currentInstructionStr=String.format("LDA <$%02X", aA&0xFF); break;
+            case 0xD6: int aB=(DP<<8)|memory.read(PC++); B=memory.read(aB); updateFlags(B); 
+                       currentInstructionStr=String.format("LDB <$%02X", aB&0xFF); break;
+            case 0x97: int sA=(DP<<8)|memory.read(PC++); memory.write(sA,A); updateFlags(A); 
+                       currentInstructionStr=String.format("STA <$%02X", sA&0xFF); break;
+            case 0xD7: int sB=(DP<<8)|memory.read(PC++); memory.write(sB,B); updateFlags(B); 
+                       currentInstructionStr=String.format("STB <$%02X", sB&0xFF); break;
+
+            // --- BRANCH ---
+            case 0x20: byte off = (byte)memory.read(PC++); PC += off; 
+                       currentInstructionStr = String.format("BRA $%02X", off); break;
+            case 0x26: byte offne = (byte)memory.read(PC++); if((CC & 0x04) == 0) PC += offne; 
+                       currentInstructionStr = String.format("BNE $%02X", offne); break;
+            case 0x27: byte offeq = (byte)memory.read(PC++); if((CC & 0x04) != 0) PC += offeq; 
+                       currentInstructionStr = String.format("BEQ $%02X", offeq); break;
 
             // --- ARITHMETIC ---
-            case 0x8B: // ADDA Imm
-                int valA = memory.read(PC++); A = (A + valA) & 0xFF; updateFlags(A);
-                currentInstructionStr = String.format("ADDA #$%02X", valA); break;
-            case 0xCB: // ADDB Imm
-                int valB = memory.read(PC++); B = (B + valB) & 0xFF; updateFlags(B);
-                currentInstructionStr = String.format("ADDB #$%02X", valB); break;
+            case 0x8B: int vA=memory.read(PC++); A=(A+vA)&0xFF; updateFlags(A); 
+                       currentInstructionStr=String.format("ADDA #$%02X", vA); break;
+            case 0xCB: int vB=memory.read(PC++); B=(B+vB)&0xFF; updateFlags(B); 
+                       currentInstructionStr=String.format("ADDB #$%02X", vB); break;
 
             // --- 16 BITS ---
-            case 0x8E: X = read16(); updateFlags16(X); currentInstructionStr = String.format("LDX #$%04X", X); break;
-            case 0xCE: S = read16(); updateFlags16(S); currentInstructionStr = String.format("LDS #$%04X", S); break;
-            case 0x8C: // CMPX
-                int valX = read16(); int resX = X - valX;
-                if (resX == 0) CC |= 0x04; else CC &= ~0x04;
-                currentInstructionStr = String.format("CMPX #$%04X", valX); break;
+            case 0x8E: X = read16(); updateFlags16(X); 
+                       currentInstructionStr = String.format("LDX #$%04X", X); break;
+            case 0xCE: S = read16(); updateFlags16(S); 
+                       currentInstructionStr = String.format("LDS #$%04X", S); break;
+            case 0x8C: int vX = read16(); int rX = X - vX; if(rX==0) CC|=4; else CC&=~4; 
+                       currentInstructionStr=String.format("CMPX #$%04X", vX); break;
 
             // --- SPECIALS ---
-            case 0x1E: // EXG
-                int pb = memory.read(PC++);
-                if (pb == 0x8B) { int t=A; A=DP; DP=t; currentInstructionStr = "EXG A,DP"; }
-                break;
-            case 0x1F: // TFR
-                int pbT = memory.read(PC++);
-                if (pbT == 0x8B) { DP = A; currentInstructionStr = "TFR A,DP"; }
-                break;
-            case 0x34: memory.read(PC++); S-=2; currentInstructionStr = "PSHS"; break;
-            case 0x35: memory.read(PC++); S+=2; currentInstructionStr = "PULS"; break;
-            case 0x7E: PC = read16(); currentInstructionStr = "JMP"; break;
-            case 0x20: PC = PC + (byte)memory.read(PC++); currentInstructionStr = "BRA"; break;
+            case 0x1E: int p1=memory.read(PC++); if(p1==0x8B){int t=A;A=DP;DP=t;currentInstructionStr="EXG A,DP";} break;
+            case 0x1F: int p2=memory.read(PC++); if(p2==0x8B){DP=A;currentInstructionStr="TFR A,DP";} break;
+            case 0x34: memory.read(PC++); S-=2; currentInstructionStr="PSHS"; break;
+            case 0x35: memory.read(PC++); S+=2; currentInstructionStr="PULS"; break;
+            case 0x7E: int tg = read16(); PC = tg; currentInstructionStr=String.format("JMP $%04X", tg); break;
             
-            case 0x4F: A=0; updateFlags(0); currentInstructionStr = "CLRA"; break;
-            case 0x5F: B=0; updateFlags(0); currentInstructionStr = "CLRB"; break;
-            case 0x3F: currentInstructionStr = "SWI (END)"; break;
-            case 0x12: currentInstructionStr = "NOP"; break;
+            // --- STOP ---
+            case 0x3F: 
+                programFinished = true; 
+                currentInstructionStr = "END"; 
+                break;
+                
+            case 0x4F: A=0; updateFlags(0); currentInstructionStr="CLRA"; break;
+            case 0x5F: B=0; updateFlags(0); currentInstructionStr="CLRB"; break;
+            case 0x12: currentInstructionStr="NOP"; break;
 
             default: currentInstructionStr = String.format("??? (%02X)", opcode); break; 
         }
@@ -120,4 +115,3 @@ public class Cpu6809 {
     public int getX() { return X; } public int getY() { return Y; }
     public int getA() { return A; } public int getB() { return B; }
     public int getDP() { return DP; } public int getCC() { return CC; }
-}
